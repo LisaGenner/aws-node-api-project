e2755bde88d8857bd0108ba41b9883083bc38544
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-10-15 22:18:42 -0500

    2.5 updates

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+const { S3Client, CopyObjectCommand } = require("@aws-sdk/client-s3");
+
+exports.handler = async (event) => {
+  // Read data from event object.
+  const region = event.Records[0].awsRegion;
+  const sourceBucket = event.Records[0].s3.bucket.name;
+  const sourceKey = event.Records[0].s3.object.key;
+
+  // Instantiate a new S3 client.
+  const s3Client = new S3Client({
+    region: region,
+  });
+
+  // Create an object with parameters for CopyObjectCommand.
+  const copyObjectParams = {
+    Bucket: process.env.DEST_BUCKET,
+    Key: sourceKey,
+    CopySource: `${sourceBucket}/${sourceKey}`,
   };
+  // Execute object copy between buckets and return the result.
+  return s3Client.send(new CopyObjectCommand(copyObjectParams));
 };
